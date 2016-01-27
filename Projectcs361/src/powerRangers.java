e8249ee27cbb86fd43e75d3089d43c2030291657
mrlacy <mrlacy@ceas-942-mac11.cae.uwm.edu>
2016-01-27 14:07:47 -0600

    cs361 - Added stuff

--- Projectcs361/src/powerRangers.java
+++ Projectcs361/src/powerRangers.java
@@ -3,7 +3,7 @@ public class powerRangers {
 	public static void main(String[] args){
 		System.out.print("Yellow ranger IN!");
 		System.out.print("Green ranger IN!");
-		System.out.println("Purple ranger IN");
+		System.out.println("Purple ranger IN!");
 	}
 
 }
