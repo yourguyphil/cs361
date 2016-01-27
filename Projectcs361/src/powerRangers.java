6bac300ff5d7367106ce4e99decb29e3ff58a324
nwhessel <hesselapplications@gmail.om>
2016-01-27 14:17:13 -0600

    rawr

--- Projectcs361/src/powerRangers.java
+++ Projectcs361/src/powerRangers.java
@@ -1,8 +1,8 @@
 
 public class powerRangers {
 	public static void main(String[] args){
-		System.out.print("Yellow ranger IN!");
-		System.out.print("Green ranger IN!");
+		System.out.println("Yellow ranger IN!");
+		System.out.println("Green ranger IN!");
 		System.out.println("Purple ranger IN!");
 		System.out.println("Black ranger IN!");
 		System.out.println("Red ranger IN!");
