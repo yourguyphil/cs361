8adf719479ed6ecdffc19d187313624c11076dee
kcerfus <kcerfus@CHS-CHERIDC-L1.soe.uwm.local>
2016-03-28 12:22:29 -0500

    TestRacer

--- src/test/race/TestRacer.java
+++ src/test/race/TestRacer.java
@@ -6,8 +6,7 @@ import java.time.LocalTime;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import race.Racer;
 
 public class TestRacer {
@@ -15,21 +14,34 @@ public class TestRacer {
 	LocalTime startTime;
 	@Before
 	public void before() {
+		racer1 = new Racer(123);
+		startTime = LocalTime.now();
 	}
 	
 	@Test
 	public void testSetStart() {
-		// TODO
+		//Racer1 hasn't started yet so startTime shouldn't exist
+		assertTrue(racer1.getStart() == null);
+		startTime = LocalTime.now();
+		racer1.setStart(startTime);
+		//Racer1 started so startTime should exist
+		assertFalse(racer1.getStart() == null);
+		//Local startTime should equal racer1s start time
+		assertEquals(startTime,racer1.getStart());
 	}
 	
 	@Test
 	public void testSetFinish() {
-		// TODO
-	}
-	
-	@Test
-	public void testCancel() {
-		// TODO
+		LocalTime finishTime = LocalTime.now();
+		racer1.setFinish(finishTime);
+		//Racer1 hasn't started so there should be no finish time
+		assertTrue(racer1.getFinish() == null);
+		racer1.setStart(startTime);
+		racer1.setFinish(finishTime);
+		// Now racer1 has started and then finished so there should be a finish time
+		assertFalse(racer1.getFinish() == null);
+		//Local finishTime should equal racer1s finish time
+		assertEquals(finishTime,racer1.getFinish());
 	}
 	
 	//Test if Did not finish will work properly
