6ac8582667f457e5021e4b2ae821c34034571a28
7373Lacym <mitch.lacy@marquette.edu>
2016-03-27 22:42:08 -0500

    Added writer def

--- src/io/Writer.java
+++ src/io/Writer.java
@@ -1,5 +1,11 @@
 package io;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /** Writes to output 
  */
 public class Writer {
@@ -9,6 +15,13 @@ public class Writer {
 	 */
 	public static void write(String contents) {
 		// write contents to 'output.txt' in the 'test.files' package
+		Path file = Paths.get("src/test.files/output.txt");
+		try (BufferedWriter writer = Files.newBufferedWriter(file)) {
+		    writer.write(contents);
+		} catch (IOException e) {
+			System.out.println("Could not print contents to output.txt");
+			e.printStackTrace();
+		}
 	}
 	
 }
