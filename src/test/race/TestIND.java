389e9b1cdd04d6abff8660822d346f3504422058
kcerfus <kcerfus@CHS-CHERIDC-L1.soe.uwm.local>
2016-03-28 11:55:45 -0500

    TestIND and testPARIND are finished.

--- src/test/race/TestIND.java
+++ src/test/race/TestIND.java
@@ -3,29 +3,58 @@ package test.race;
 import static org.junit.Assert.*;
 
 import java.time.LocalTime;
-import java.util.Collections;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import race.IND;
-import race.Racer;
 
 public class TestIND {
-	
+	LocalTime time;
 	@Before
 	public void before() {
-		
+		time = LocalTime.now();
 	}
 	
 	@Test
 	public void testStart() {
-		// TODO
+		IND ind = new IND();
+		ind.num(123);
+		ind.num(234);
+		ind.start(time, 12);
+		//Bad lanes, shouldn't work
+		assertNull(ind.getLane(12));
+		assertNull(ind.getLane(1));
+		
+		ind.start(time,0);
+		//Started one racer so start queue should have 1 
+		assertEquals(1,ind.getLane(0).getStartedRacers().size());
+		//Started another so start queue should now have 2
+		ind.start(time, 0);
+		assertEquals(2,ind.getLane(0).getStartedRacers().size());
 	}
 	
 	@Test
 	public void testSwap() {
-		// TODO
+		//Put 3 racers into pendingRacers queue
+		IND ind = new IND();
+		ind.num(123);
+		ind.num(234);
+		ind.num(345);
+
+		//Start 3 racers 
+		time = LocalTime.now();
+		ind.start(time, 0);
+		assertTrue(ind.getLane(0).getStartedRacers().peek().getBib() == 123);
+		time = LocalTime.now();
+		ind.start(time, 0);
+		ind.start(time, 0);
+		ind.clear(123);
+		//swap 1st two racers, 1st place should now be 234 and second should be 123
+		ind.swap();
+
+		assertFalse(ind.getLane(0).getStartedRacers().peek().getBib() == 123);
+		assertTrue(ind.getLane(0).getStartedRacers().peek().getBib() == 234);
 	}
 	
 }
--- src/test/race/TestPARIND.java
+++ src/test/race/TestPARIND.java
@@ -1,17 +1,44 @@
 package test.race;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.time.LocalTime;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import race.IND;
+import race.PARIND;
+
+
 public class TestPARIND {
-	
+	LocalTime time;
 	@Before
 	public void before() {
-		
+		time = LocalTime.now();
 	}
 	
 	@Test
 	public void testStart() {
-		// TODO Test should be the same as the testStart in IND
+		PARIND parind = new PARIND();
+		// 3 racers, 123 and 234 are in Lane 0 while 345 will be in lane 1
+		parind.num(123);
+		parind.num(234);
+		parind.num(345);
+		parind.start(time, 12);
+		//Bad lane, shouldn't work
+		assertNull(parind.getLane(12));
+		
+		parind.start(time,0);
+		//Started one racer so start queue should have 1 
+		assertEquals(1,parind.getLane(0).getStartedRacers().size());
+		assertEquals(0,parind.getLane(1).getStartedRacers().size());
+		//Started another so start queue should now have 2
+		parind.start(time, 0);
+		assertEquals(2,parind.getLane(0).getStartedRacers().size());
+		parind.start(time,1);
+		//Lane 1 should now have 1 startedRacer
+		assertEquals(1,parind.getLane(1).getStartedRacers().size());
 	}
 }
